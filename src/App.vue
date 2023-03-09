c163a727aedb95881d60eb9537a7fe704c45108d
Analia <analiambenavente@gmail.com>
2023-03-09 18:48:36 +0100

    primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="logo Pokemon" src="./assets/logoPokemon.jpeg" />
+    <div>
+      <b-table striped hover :items="listaPokemon.results" :fields="fields">
+        <template #cell(name)="data">
+          <h4 @click="getdetallesPokemon(data.item.url)">{{data.item.name.toUpperCase()}}</h4>
+        </template>
+      </b-table>
+      <CardModal :detallesPokemon="detallesPokemon" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CardModal from "./components/CardModal.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    CardModal
+  },
+  data() {
+    return {
+      listaPokemon: [],
+      fields: ["name"],
+      detallesPokemon: []
+    };
+  },
+
+  methods: {
+    async getListaPokemon() {
+      try {
+        const lista = await fetch("https://pokeapi.co/api/v2/pokemon");
+        this.listaPokemon = await lista.json();
+        console.log(this.listaPokemon);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getdetallesPokemon(url) {
+      try {
+        const data = await fetch(url);
+        this.detallesPokemon = await data.json();
+        console.log(this.detallesPokemon);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.$bvModal.show("cardPokemon");
+      }
+    }
+  },
+  created() {
+    this.getListaPokemon();
   }
-}
+};
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +63,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h4 {
+  font-weight: bold;
+  color: rgb(225, 163, 69);
+  cursor: pointer;
 }
 </style>
---
+++ src/components/CardModal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <b-modal id="cardPokemon" :title="detallesPokemon.name">
+      <img :src="detallesPokemon.sprites.front_default" :alt="detallesPokemon.name" />
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardModal",
+  props: {
+    detallesPokemon: Array
+  }
+};
+</script>
+
+<style>
+</style>
